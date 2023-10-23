925ddcbe9f2cf89924936547eb14be0301e80cbd
Xeliul <xeliusone@gmail.com>
2023-10-22 22:35:08 -0400

    collisions

--- js/canvas.js
+++ js/canvas.js
@@ -1,101 +1,111 @@
 let canvas = document.getElementById("canvas1");
-let ctx = canvas.getContext('2d');
+let ctx    = canvas.getContext('2d');
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 
-function drawBall (a, b) {
+const BALLRADIUS = 10;
+let dx           = 2,
+	dy           = -2;
+
+//Brick defs
+const BRICKROWCOUNT = 3,
+	  BRICKCOLUMNCOUNT = 5,
+	  BRICKWIDTH = 75,
+	  BRICKHEIGHT = 20,
+	  BRICKPADDING = 10,
+	  BRICKOFFSETTOP = 30,
+	  BRICKOFFSETLEFT = 30;
+
+//Paddle defs
+const PADDLEHEIGHT = 10;
+const PADDLEWIDTH  = 75;
+let paddleX        = (canvas.width - PADDLEWIDTH) / 2;
+
+function drawBall() {
 	ctx.beginPath();
-	ctx.arc(a, b, 15, 0, Math.PI * 2);
-	ctx.fillStyle = "#0095DD"
+	ctx.arc(x, y, BALLRADIUS, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
 }
 
-function rand () {
-	return Math.floor(Math.pow(Math.random() * 10, 2.5 * Math.ceil(Math.random(Math.pow(Math.random(), Math.random())))));
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height - PADDLEHEIGHT, PADDLEWIDTH, PADDLEHEIGHT);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
 }
 
-//Generate the list of coordinate sets.
-let cords = [[rand(), rand()], [rand(), rand()], [rand(), rand()]]
+let rightPressed = false,
+	leftPressed  = false;
 
-function genPath (p1, p2, p3){
-	// let [p1, p2, p3] = [cords[0], cords[1], cords[2]]
-	let s1 = coordSets(p1, p2),
-		s2 = coordSets(p2, p3),
-		runSet = [[],[]];
-	let a = s1[0], b = s2[0];
-	a.push.apply(a, b);
-	runSet[0].push(...a);
-	
-	a = s1[1]; b = s2[1];
-	a.push.apply(a, b);
-	runSet[1].push(...a);
-	
-	// console.log(`S1[0]: ${s1[0]}\nS2[0]: ${s2[0]}\n\n${runSet}\n\n----------BREAK----------\n\n`)
-	// console.log(`S1 = ${s1[0].length}\n${s1[1].length}\n\nS2 = ${s2[0].length}\n${s2[1].length}\n\n\n\nRunset = ${runSet[0]}\n${runSet[1]}`)
-	console.log(runSet[0][0][0])
-	return runSet;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.key === "Right" || e.key === "ArrowRight") {
+		rightPressed = true;
+	}
+	else if(e.key === "Left" || e.key === "ArrowLeft") {
+		leftPressed = true;
+	}
+}
+function keyUpHandler(e) {
+	if(e.key === "Right" || e.key === "ArrowRight") {
+		rightPressed = false;
+	}
+	else if(e.key === "Left" || e.key === "ArrowLeft") {
+		leftPressed = false;
+	}
 }
 
-function coordSets (curr, dest) {
-	let path = [[],[]];
-	path[0].push(curr[0]);
-	path[1].push(curr[1]);
+const BRICKS = [];
+for (let c = 0; c < BRICKCOLUMNCOUNT; c++) {
+	bricks[c] = [];
+	for (let r = 0; r < BRICKROWCOUNT; r++) {
+		bricks[c][r] = { x: 0, y: 0 };
+	}
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	drawPaddle();
 	
-	let [x, y] = [curr[0], curr[1]];
-	
-	let bigDiff;
-	{
-		let xDiff = -Math.abs(curr[0]) + Math.abs(dest[0]);
-		let yDiff = -Math.abs(curr[1]) + Math.abs(dest[1]);
-		
-		if (xDiff > yDiff) {
-			bigDiff = xDiff
+	if (x + dx > canvas.width - BALLRADIUS || x + dx < BALLRADIUS) {
+		dx = -dx;
+	}
+	if (y + dy < BALLRADIUS) {
+		dy = -dy;
+	} else if (y +dy > canvas.height - BALLRADIUS) {
+		if (x > paddleX && x < paddleX + PADDLEWIDTH) {
+			dy = -dy;
 		} else {
-			bigDiff = yDiff
+			alert("GAME OVER");
+			document.location.reload();
+			clearInterval(interval);
 		}
 	}
 	
-	//Find biggest difference and set bigDiff.
-	for (let i=0; i<bigDiff; i++) {
-		//check x
-		//if more
-		if(x > dest[0])
-			x--;
-		//if less
-		if(x < dest[0])
-			x++;
-		//push x
-		path[0].push(x);
-		
-		//check y
-		//if more
-		if(y > dest[1])
-			y--;
-		//if less
-		if(y < dest[1])
-			y++;
-		//push y
-		path[1].push(y);
+	x += dx;
+	y += dy;
+	
+	if(rightPressed) {
+		paddleX = Math.min(paddleX + 10, canvas.width - PADDLEWIDTH);
+		if (paddleX + paddleWidth > canvas.width){
+			paddleX = canvas.width - paddleWidth;
+		}
 	}
-	return path;
-}
-
-//Check if you at a coordinate set.so
-function isThere (curCord, destCord) {so
-	if(curCord[0] == destCord[0] && curCord[1] == destCord[1]) return true;
-}
-
-let runSet = genPath(cords[0], cords[1], cords[2]);
-console.log(runSet[0])
-console.log(runSet[1])
-
-function draw () {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawBall(runSet[0].shift(), runSet[1].shift());
+	else if(leftPressed) {
+		paddleX -= 10;
+		if (paddleX < 0){
+			paddleX = 0;
+		}
+	}
+	
 }
 
 
-
-setInterval(draw, 25);
+const interval = setInterval(draw, 10);
