a949c436c2b300c8af48e983ba332f902f0febf0
Xeliul <xeliusone@gmail.com>
2023-11-09 18:33:27 -0500

    Installed bevy.rs

---
+++ bevy_egine/target/debug/bevy_egine.d
@@ -0,0 +1,1 @@
+C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\target\debug\bevy_egine.exe: C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\src\main.rs
---
+++ bevy_egine/target/debug/deps/bevy_egine.d
@@ -0,0 +1,5 @@
+C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\target\debug\deps\bevy_egine.exe: src\main.rs
+
+C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\target\debug\deps\bevy_egine.d: src\main.rs
+
+src\main.rs:
