43d6eb3d13a70f0854fc8c81e27d5ca65bf647a2
Xeliul <xeliusone@gmail.com>
2023-11-09 22:57:18 -0500

    Shapes..

--- bevy_egine/src/main.rs
+++ bevy_egine/src/main.rs
@@ -1,63 +1,53 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 fn main() {
 	App::new()
-		.add_plugins((DefaultPlugins, HelloPlugin))
+		.add_plugins(DefaultPlugins)
+		.add_systems(Startup, setup)
 		.run();
 }
 
-#[derive(Component)]
-struct Person;
+fn setup (
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<ColorMaterial>>
+) {
+	commands.spawn(Camera2dBundle::default());
 
-#[derive(Component)]
-struct Name(String);
+	//Circle
+	commands.spawn(MaterialMesh2dBundle{
+		mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+		material: materials.add(ColorMaterial::from(Color::PURPLE)),
+		transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+		..default()
+	});
 
-fn ap(mut commands: Commands) {
-	commands.spawn((Person, Name("Albert".to_string())));
-	commands.spawn((Person, Name("Renzo".to_string())));
-	commands.spawn((Person, Name("Zayna".to_string())));
+	// Rectangle
+	commands.spawn(SpriteBundle {
+		sprite: Sprite {
+			color: Color::rgb(0.25, 0.25, 0.75),
+			custom_size: Some(Vec2::new(50.0, 100.0)),
+			..default()
+		},
+		transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
+		..default()
+	});
+
+	// Quad
+	commands.spawn(MaterialMesh2dBundle {
+		mesh: meshes
+			.add(shape::Quad::new(Vec2::new(25., 100.)).into())
+			.into(),
+		material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+		transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
+		..default()
+	});
+
+	// Hexagon
+	commands.spawn(MaterialMesh2dBundle {
+		mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
+		material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
+		transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
+		..default()
+	});
 }
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn gp(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-	// update our timer with the time elapsed since the last update
-	// if that caused the timer to finish, we say hello to everyone
-	if timer.0.tick(time.delta()).just_finished() {
-		for name in &query {
-			println!("Hello {}!", name.0);
-		}
-	}
-}
-
-//PLUGIN
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-	fn build(&self, app: &mut App) {
-		app
-			.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-			.add_systems(Startup, ap)
-			.add_systems(Update, (gp));
-	}
-}
-
-
-
-
-
-
-/*
-// Components: Rust structs that implement the Component trait
-#[derive(Component)]
-struct Position { x: f32, y: f32 }
- //Systems: normal Rust functions
-fn print_position_system(query: Query<&Position>) {
-	 for position in &query {
-		 println!("position: {} {}", position.x, position.y);
-	 }
- }
-//Entities: a simple type containing a unique integer
-struct Entity(u64);
-*/
