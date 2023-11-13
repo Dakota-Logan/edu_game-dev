aabde28cba701fdc0b0aaa3c47b21018c07bf437
Xeliul <xeliusone@gmail.com>
2023-11-12 20:59:27 -0500

    Was told to avoid Vulkan if I'm not comfortable with computer graphics prior, so I'll learn something else I guess.

--- bevy_egine/target/debug/bevy_egine.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\target\debug\bevy_egine.exe: C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\src\main.rs
--- bevy_egine/target/debug/deps/bevy_egine.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\target\debug\deps\bevy_egine.exe: src\main.rs
-
-C:\Users\Xeliu\CLionProjects\games_learnign\bevy_egine\target\debug\deps\bevy_egine.d: src\main.rs
-
-src\main.rs:
