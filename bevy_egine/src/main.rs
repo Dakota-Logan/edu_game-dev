1e8a39c3f93f2f0118a4c2597f2eac0616ac76c7
Xeliul <xeliusone@gmail.com>
2023-11-17 12:06:43 -0500

    Moving unto 2d graphics libraries with prebuilt crates, will see how it works out. Would like to start one level above graphics apis (dx11, vulkan), but not all the way to simple 1 method calls to draw a the whole scene.

--- bevy_egine/src/main.rs
+++ bevy_egine/src/main.rs
@@ -1,6 +1,7 @@
 use winit::{
 	event_loop::EventLoop,
 	window::{Window, WindowBuilder},
+	dpi::LogicalPosition,
 };
 
 fn main() {
@@ -8,9 +9,11 @@ fn main() {
 	let window_builder = WindowBuilder::new()
 		.with_theme(Some(winit::window::Theme::Dark))
 		.with_title("My Window")
-		.with_inner_size(winit::dpi::LogicalSize::new(800., 600.));
+		.with_inner_size(winit::dpi::LogicalSize::new(800., 600.))
+		.with_fullscreen(Some(windowed_context.get_primary_monitor()));
 
 	let window  = window_builder.build(&event_loop).unwrap();
+	window.set_outer_position(LogicalPosition::new(400., 400.));
 
 	event_loop.run(move |event, _, control_flow| {
 		*control_flow = winit::event_loop::ControlFlow::Wait;
