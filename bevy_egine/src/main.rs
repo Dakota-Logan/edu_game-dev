6ecdea7962ee9466c3f6867ed8c3eb31e6fb7e85
Xeliul <xeliusone@gmail.com>
2023-11-17 11:04:43 -0500

    Separating window_builder information from the actual build, added theme and size.

--- bevy_egine/src/main.rs
+++ bevy_egine/src/main.rs
@@ -5,17 +5,22 @@ use winit::{
 
 fn main() {
 	let event_loop = EventLoop::new();
-	let window = WindowBuilder::new().build(&event_loop).unwrap();
+	let window_builder = WindowBuilder::new()
+		.with_theme(Some(winit::window::Theme::Dark))
+		.with_title("My Window")
+		.with_inner_size(winit::dpi::LogicalSize::new(800., 600.));
+
+	let window  = window_builder.build(&event_loop).unwrap();
 
 	event_loop.run(move |event, _, control_flow| {
 		*control_flow = winit::event_loop::ControlFlow::Wait;
 
 		match event {
-			winit::event::Event::WindowEvent {
-				event: winit::event::WindowEvent::CloseRequested,
-				..
-			} => *control_flow = winit::event_loop::ControlFlow::Exit,
-			_ => (),
+			winit::event::Event::WindowEvent { event, .. } => match event {
+				winit::event::WindowEvent::CloseRequested => *control_flow = winit::event_loop::ControlFlow::Exit,
+				_ => (),
+			},
+			_ => ()
 		}
 	});
 }
